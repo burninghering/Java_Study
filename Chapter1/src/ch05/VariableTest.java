066e7c6693da70ac2abd393c38a85c236d563a68
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-05 23:16:23 +0900

    220105 VariableTest

---
+++ Chapter1/src/ch05/VariableTest.java
@@ -0,0 +1,13 @@
+package ch05;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age, count;
+		age = 10; /* age는 R value, 10은 L value -> 대입 연산 */
+		
+		int level = 10000;
+		System.out.println(age);
+		System.out.println(level);
+	}
+}
