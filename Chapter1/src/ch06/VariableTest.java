570e205c456657bbe1cc703506c7c60eeb7e2df2
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-06 09:46:18 +0900

    java data type - double

--- Chapter1/src/ch06/VariableTest.java
+++ Chapter1/src/ch06/VariableTest.java
@@ -7,7 +7,10 @@ public class VariableTest {
 		
 		System.out.println(bnum);
 		
-		int num = 12345678900;
+		int num = 12345678900; //너무 크니까 줄여라!
+		long lnum = 12345678900; //리터럴이 너무 크니까 줄여라!
+		long lnumber = 12345678900L; //잘했다
+		
+		long okay_num = 12345678; //long 범위 안에 맞는 리터럴을 넣으면 에러 X(알아서 int,4byte로 변환되기 때문)
 	}
-
 }
---
+++ Chapter1/src/ch07/DoubleTest.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class DoubleTest {
+	public static void main(String[] args) {
+		
+		double dnum=3.14;
+		float fnum=3.14F; //리터럴은 지정된 데이터 타입(int)으로 저장 -> 4byte -> 3.14는 4byte에 담긴 너무 크다  -> f붙이기
+		//만약 파라미터 에러가 날 때,  F를 안 붙여줘서 에러가 나는 경우가 많음 
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+	}
+
+}
