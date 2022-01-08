3328e7d75cb573a9a70db980ff660c0e67fe48e6
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-08 18:59:03 +0900

    ~참조 자료형

--- Chapter2/src/ch06/Student.java
+++ Chapter2/src/ch06/Student.java
@@ -5,7 +5,22 @@ public class Student {
 	public String studentName;
 	public int grade;
 
+	public Student() {
+	}
+
+	public Student(int studentNumber, String studentName, int grade) {
+
+		// studentNumber=studentNumber;
+		// 변수는 가장 가까운 변수를 찾기 때문에, 매개 변수에다가 매개 변수를 넣게 된다.
+		// 우리는 멤버 변수에 넘어온 값을 대입하고 싶다 (=생성자의 역할)
+
+		this.studentNumber = studentNumber;
+		this.studentName = studentName;
+		this.grade = grade;
+	}
+
 	public String showStudentInfo() {
 		return studentName + "학생의 학번은" + studentNumber + "이고, " + grade + "학년입니다.";
 	}
+
 }
--- Chapter2/src/ch06/StudentTest.java
+++ Chapter2/src/ch06/StudentTest.java
@@ -1,5 +1,16 @@
 package ch06;
 
 public class StudentTest {
-
+	public static void main(String[] args) {
+		
+		Student studentLee=new Student(); //디폴트 생성자
+		//클래스에 생성자가 하나도 없는 경우 컴파일러가 생성자 코드를 넣어 주는데
+		//우리가 생성자를 만들어버렸다.
+		
+		System.out.println(studentLee.showStudentInfo());
+	
+		Student studentKim=new Student(123456,"Kim",3);
+		System.out.println(studentKim.showStudentInfo());
+	}
 }
+
