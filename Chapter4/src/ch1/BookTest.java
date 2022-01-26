8055add6a3c21f2d34d4fb17c71ceb05f83935a8
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-26 13:21:28 +0900

    Object Class

---
+++ Chapter4/src/ch1/BookTest.java
@@ -0,0 +1,34 @@
+package ch1;
+
+class Book{
+	
+	private String title;
+	private String author;
+	
+	public Book(String title, String author) {
+		this.title=title;
+		this.author=author;
+	}
+
+	@Override
+	public String toString() {
+		return title+","+author;
+	}
+	
+	
+}	
+	
+	
+	
+public class BookTest{
+	public static void main(String[] args) {
+		
+		Book book=new Book("데미안","헤르만 헤세");
+		
+		System.out.println(book.toString());
+		
+		String str = new String("test"); //toString()을 이미 오버라이딩해서 쓰고 있음
+		
+		System.out.println(str); //toString()이 뒤에 제외됨
+		}
+	}	
