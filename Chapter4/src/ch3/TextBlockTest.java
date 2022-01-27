5816d32b236396d450ea42cf1440c3dc21614417
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-28 00:04:39 +0900

    comment out

--- Chapter4/src/ch3/TextBlockTest.java
+++ Chapter4/src/ch3/TextBlockTest.java
@@ -2,9 +2,24 @@ package ch3;
 
 public class TextBlockTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		public static String getBlockOfHtml() {
+		    return """
+		            <html>
 
+		                <body>
+		                    <span>example text</span>
+		                </body>
+		            </html>""";	
 	}
-
+		
+		public static void main(String[] args) {
+			String textBlocks="""
+					Hello,
+					Hi,
+					How r u
+					""";
+			
+			System.out.println(textBlocks);
+			System.out.println(getBlockOfHtml());
+		}
 }
