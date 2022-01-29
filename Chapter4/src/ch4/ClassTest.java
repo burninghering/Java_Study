5ef6a8a33e770bd438c7d09a35400857c519b537
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-29 23:30:44 +0900

    Class class - 생성자 만들기

--- Chapter4/src/ch4/ClassTest.java
+++ Chapter4/src/ch4/ClassTest.java
@@ -13,16 +13,19 @@ public class ClassTest {
 		person.setName("Lee");
 		System.out.println(person);
 		
-		
+//여기서부터				
 		Class c2 = person.getClass(); // getClass()를 쓰려면 인스턴스가 있어야 한다.(우리는 person이 있다)
 		Person p=(Person)c2.newInstance(); //기본 생성자가 불려진다.
 		System.out.println(p); //null 리턴
 		
+
 		Class[] parameterTypes = {String.class}; //2. Class array로, String.class를 주자
 		Constructor cons=c2.getConstructor(parameterTypes); //1. getConstructor를 쓰려면 파라미터 타입이 무엇인지 정해 인자를 넣어줘야 한다.
 		
 		Object[] initargs= {"Kim"}; //2. Object array로 {"Kim"} 주기
 		Person KimPerson = (Person)cons.newInstance(initargs); //1. newInstance를 쓰려면 초기값이 필요하다
 		System.out.println(KimPerson);
+//여기까지가 아래 코드와 같다.
+		Person kim2=new Person("Kim");
 	}
 }
