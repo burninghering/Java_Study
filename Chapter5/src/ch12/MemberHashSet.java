f56e770a0a71042d435a606c9619cfe52bcea988
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-02-22 17:34:50 +0900

    HashSet

---
+++ Chapter5/src/ch12/MemberHashSet.java
@@ -0,0 +1,56 @@
+package ch12;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class MemberHashSet {
+	private HashSet<Member> hashSet; //<>안에 실제로 관리할 것을 넣으면 됨
+	
+	public MemberHashSet() {
+		hashSet=new HashSet<>();
+	}
+	
+	public MemberHashSet(int size) {
+		hashSet=new HashSet<>(size);
+	}
+	
+	public void addMember(Member member) {
+		hashSet.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+//		for(int i=0; i<arrayList.size();i++) {
+//			Member member = arrayList.get(i);
+//			
+//			int tempId = member.getMemberId();
+//			if (tempId==memberId) {
+//				arrayList.remove(i);
+//				return true;
+//			}	
+//		}
+		
+		Iterator<Member> ir = hashSet.iterator(); //<>에 아무것도 안쓰면 Object가 된다!
+		//내가 이터레이션할 요소가 Member이기 때문에 <>안에 넣는다
+		
+		while(ir.hasNext()) { //ir에 다음이 있는가?
+			
+			Member member = ir.next();
+			
+			int tempId = member.getMemberId();
+			if (tempId==memberId) {
+				hashSet.remove(member); //i를 삭제하면 안되고 오브젝트를 삭제해야 한다.
+				return true;
+			}
+		}
+			System.out.println(memberId+"가 존재하지 않습니다.");
+			return false;
+		}
+	
+	
+	public void showAllMember() {
+		for(Member member : hashSet) {
+			System.out.println(member);
+		}
+		System.out.println();
+	}
+}
