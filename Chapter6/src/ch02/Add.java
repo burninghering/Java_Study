f8be0a2e75195f8a54fa3e1e5a27da4004986ecb
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-02-26 19:33:41 +0900

    @FunctionalInterface

--- Chapter6/src/ch02/Add.java
+++ Chapter6/src/ch02/Add.java
@@ -1,6 +1,8 @@
 package ch02;
 
-public interface Add { //람다식을 쓰기 전, 인터페이스를 만들고
+@FunctionalInterface
+public interface Add {
 
-	public int add(int x, int y); //함수를 구현해놓는다.
+	public int add(int x, int y); 
+
 }
--- Chapter6/src/ch02/AddTest.java
+++ Chapter6/src/ch02/AddTest.java
@@ -3,11 +3,10 @@ package ch02;
 public class AddTest {
 
 	public static void main(String[] args) {
-		Add addL = ( x, y )-> {return x+y;}; //Add가 구현되었다!
+		Add addL = ( x, y )-> {return x+y;}; //인터페이스의 무슨 함수를 사용했는지 모호해진다.
 		Add addM= ( x,y )-> x+y;
 		
 		System.out.println(addL.add(2, 3));
 		System.out.println(addM.add(2, 3));
 	}
-
 }
